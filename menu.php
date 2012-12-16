de7ebb898294745127e1e358aea40de64d67b4c8
petunio4 <petunio4@gmail.com>
2012-12-16 14:14:09 -0600

    f

---
+++ menu.php
@@ -0,0 +1,33 @@
+    <nav>
+      <ul id="nav">
+       <li><a href="index.php" <?php if($_SERVER['PHP_SELF']== '/pinkstreet/index.php'){ ;?> class="current" <?php }?> >Inicio</a></li>
+      </ul>
+      <ul id="nav">
+        <li><a href="desayunos.php" <?php if($_SERVER['PHP_SELF']== '/pinkstreet/desayunos.php'){ ;?> class="current" <?php }?> >Desayunos</a></li>
+      </ul>
+      <ul id="nav">
+       <li><a href="comidas.php" <?php if($_SERVER['PHP_SELF']== '/pinkstreet/comidas.php'){ ;?> class="current" <?php }?> >Comidas</a></li>
+      </ul>
+      <ul id="nav">
+       <li><a href="#">Cenas</a></li>
+      </ul>
+      <ul id="nav">
+       <li><a href="#">Box Lunch</a></li>
+      </ul>
+      <ul id="nav">
+       <li><a href="#">Box Lunch Kinder</a></li>
+      </ul>
+      <ul id="nav">
+       <li><a href="#">Otros regalos</a></li>
+      </ul>
+      <ul id="nav">
+        <li><a href="#">Experiencias</a></li>
+      </ul>
+      <ul id="nav">
+       <li><a href="#">Tips Nutricion</a></li>
+      </ul>
+      <ul id="nav">
+       <li><a href="#">Boutique</a></li>
+      </ul>
+      
+    </nav>
